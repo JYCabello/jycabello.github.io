d61514b238e1c80528c4a5ffc4f24228d7f7b0be
JYCabello <j.cabello.ramos@gmail.com>
2026-06-07 18:52:25 +0000

    From commit 770539c9ab8f2b285bd9af077c3ff41d39261953

--- release.sh
+++
@@ -1,8 +0,0 @@
-bundle exec jekyll build
-cd ../jycabello.github.io/
-git checkout master
-rm -r *
-cp -rf ../jycabello-jekyll/_site/* .
-git add .
-git commit -m "Released from local machine"
-git push origin master
